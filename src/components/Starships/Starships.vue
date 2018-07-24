8d6c16558c703602c3422eca343488c9e0aeb79d
Floriza Abreu <contato.denysrocha@gmail.com>
2018-07-23 23:19:23 -0300

    getting better the apperence

--- src/components/Starships/Starships.vue
+++ src/components/Starships/Starships.vue
@@ -1,9 +1,27 @@
 <template>
-  <div>
-    <input type="text" name="" value="" v-model="distance">
-    <button type="button" name="button" v-on:click="calculateStops">Calculate stops</button>
-    <br>
-    <div v-for="r in results">Starship: {{ r.name }} - MGLT: {{ r.MGLT }} - stops: {{ r.stops }}</div>
+  <div class="container well">
+  	<div class="row">
+  		<div class="col-md-12">
+  			<label for="distance">Inform the distance to be traveled</label>
+  		</div>
+  	</div>
+  	<div class="row">
+  		<div class="col-md-9">
+  			<input class="form-control" type="text" name="distance" v-model="distance">
+  		</div>
+  		<div class="col-md-3">
+  			<button class="btn btn-info pull-right" type="button" name="button" v-on:click="calculateStops">Calculate stops</button>		
+  		</div>
+  	</div>
+  	<hr>
+    <div class="row" v-for="r in results">
+		<div class="col-md-6">
+			Starship: {{ r.name }}	
+		</div>
+		<div class="col-md-2">
+			Stops: {{ r.stops }}
+		</div> 
+	</div>
 
   </div>
 </template>
@@ -13,5 +31,14 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+.container{
+	max-width: 600px;
+}
+
+.well{
+	background-color: rgba(255, 255, 255, 0.9);
+	box-shadow: 0px 3px 10px rgba(255, 255, 255, 0.7);
+}
 
 </style>
